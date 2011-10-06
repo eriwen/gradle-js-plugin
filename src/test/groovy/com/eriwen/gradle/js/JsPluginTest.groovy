3ac059be13966b772000fce996be1327a995d3c6
Eric Wendelin <emwendelin@gmail.com>
2011-10-06 06:33:55 -0600

    Adding tests

--- src/test/groovy/com/eriwen/gradle/js/JsPluginTest.groovy
+++ src/test/groovy/com/eriwen/gradle/js/JsPluginTest.groovy
@@ -7,6 +7,7 @@ import org.junit.After
 import org.junit.Test
 
 import static org.junit.Assert.*
+import org.gradle.api.tasks.TaskExecutionException
 
 class JsPluginTest {
     private static final String TEST_SOURCE_PATH = new File('.', 'src/test/resources').absolutePath
@@ -33,6 +34,22 @@ class JsPluginTest {
         assertEquals 1, project.getTasksByName('combineJs', false).size()
         assertEquals 1, project.getTasksByName('minifyJs', false).size()
         assertEquals 1, project.getTasksByName('gzipJs', false).size()
+        assertEquals 1, project.getTasksByName('processJs', false).size()
         assertEquals 0, project.getTasksByName('bogus', false).size()
+    }
+
+    @Test(expected=TaskExecutionException.class)
+    void shouldFailOnDownstreamTaskFailure() {
+        plugin.apply(project)
+
+        JsPluginConvention pluginConvention = plugin.jsPluginConvention
+        pluginConvention.with {
+            // combine task will fail with no input or output
+            input = null
+            output = null
+        }
+        project.getTasksByName('combineJs', false).iterator().next().execute()
+
+        fail 'Should have gotten a TaskExecutionException'
     }
 }

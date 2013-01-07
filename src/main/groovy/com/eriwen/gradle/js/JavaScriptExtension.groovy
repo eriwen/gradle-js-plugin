a8ad700e6ca673265af8c9fe40a96cdea04d0197
Eric Wendelin <emwendelin@gmail.com>
2013-01-07 14:45:34 -0700

    Remove Gradle/Google resolver due to incompatibility with Gradle 1.3. Fixes #41

--- src/main/groovy/com/eriwen/gradle/js/JavaScriptExtension.groovy
+++ src/main/groovy/com/eriwen/gradle/js/JavaScriptExtension.groovy
@@ -1,10 +1,5 @@
 package com.eriwen.gradle.js
 
-import org.gradle.api.artifacts.repositories.ArtifactRepository
-import org.gradle.api.artifacts.repositories.IvyArtifactRepository
-import org.gradle.api.artifacts.repositories.MavenArtifactRepository
-import org.gradle.api.internal.artifacts.ResolverFactory
-import org.gradle.api.internal.artifacts.repositories.layout.PatternRepositoryLayout
 import org.gradle.api.Project
 import org.gradle.util.ConfigureUtil
 import com.eriwen.gradle.js.source.JavaScriptSourceSetContainer
@@ -14,10 +9,6 @@ import com.eriwen.gradle.js.source.internal.InternalGradle
 class JavaScriptExtension {
     public static final NAME = "javascript"
 
-    public static final String GRADLE_PUBLIC_JAVASCRIPT_REPO_URL = "http://repo.gradle.org/gradle/javascript-public"
-    public static final String GOOGLE_APIS_REPO_URL = "http://ajax.googleapis.com/ajax/libs"
-
-    private final ResolverFactory resolverFactory
     final JavaScriptSourceSetContainer source
 
     JavaScriptExtension(Project project) {
@@ -26,26 +17,5 @@ class JavaScriptExtension {
 
     void source(Closure closure) {
         ConfigureUtil.configure(closure, source)
-    }
-
-    public ArtifactRepository getGradlePublicJavaScriptRepository() {
-        MavenArtifactRepository repo = resolverFactory.createMavenRepository()
-        repo.setUrl(GRADLE_PUBLIC_JAVASCRIPT_REPO_URL)
-        repo.setName("Gradle Public JavaScript Repository")
-        return repo
-    }
-
-    public ArtifactRepository getGoogleApisRepository() {
-        IvyArtifactRepository repo = resolverFactory.createIvyRepository()
-        repo.setName("Google Libraries Repository")
-        repo.setUrl(GOOGLE_APIS_REPO_URL)
-        repo.layout("pattern", new Closure(this) {
-            public void doCall() {
-                PatternRepositoryLayout layout = (PatternRepositoryLayout) getDelegate()
-                layout.artifact("[organization]/[revision]/[module].[ext]")
-                layout.ivy("[organization]/[revision]/[module].xml")
-            }
-        })
-        return repo
     }
 }

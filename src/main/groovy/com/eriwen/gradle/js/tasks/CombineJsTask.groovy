3ac579ac371dcaa2153a035828fbd39b4a9e9fe6
user <user@localhost>
2014-09-05 17:10:29 +0200

    made encoding configurable.

--- src/main/groovy/com/eriwen/gradle/js/tasks/CombineJsTask.groovy
+++ src/main/groovy/com/eriwen/gradle/js/tasks/CombineJsTask.groovy
@@ -17,18 +17,21 @@ package com.eriwen.gradle.js.tasks
 
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.SourceTask
 
 class CombineJsTask extends SourceTask {
     @OutputFile def dest
+
+    @Input encoding = System.properties['file.encoding']
 
     File getDest() {
         project.file(dest)
     }
 
     @TaskAction
-    def run() {
-        ant.concat(destfile: (dest as File).canonicalPath, fixlastline: 'yes') {
+    def run() { 
+        ant.concat(destfile: (dest as File).canonicalPath, fixlastline: 'yes', encoding: encoding) {
             source.files.each {
                 logger.info("Adding to fileset: ${it}")
                 fileset(file: it)

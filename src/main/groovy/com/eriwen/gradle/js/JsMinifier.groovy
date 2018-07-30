76a0a83d446d5949dffb9f0908cb5a7208dcd712
Thorsten Möller <thorsten.moeller@sbi.ch>
2018-07-30 10:32:09 +0200

    Refactor: Align with API of Google Closure compiler v20180716.

--- src/main/groovy/com/eriwen/gradle/js/JsMinifier.groovy
+++ src/main/groovy/com/eriwen/gradle/js/JsMinifier.groovy
@@ -1,13 +1,14 @@
 package com.eriwen.gradle.js
 
+import org.gradle.api.GradleException
+
 import com.google.javascript.jscomp.CommandLineRunner
 import com.google.javascript.jscomp.CompilationLevel
 import com.google.javascript.jscomp.Compiler
 import com.google.javascript.jscomp.CompilerOptions
-import com.google.javascript.jscomp.SourceFile
 import com.google.javascript.jscomp.Result
+import com.google.javascript.jscomp.SourceFile
 import com.google.javascript.jscomp.WarningLevel
-import org.gradle.api.GradleException
 
 /**
  * Util to minify JS files with Google Closure Compiler.
@@ -25,12 +26,12 @@ class JsMinifier {
         CompilationLevel.valueOf(compilationLevel).setOptionsForCompilationLevel(options)
         WarningLevel level = WarningLevel.valueOf(warningLevel)
         level.setOptionsForWarningLevel(options)
-        List<SourceFile> externs = CommandLineRunner.getBuiltinExterns(new CompilerOptions());
+        List<SourceFile> externs = CommandLineRunner.getBuiltinExterns(options.environment)
         if (externsFiles.size()) {
             externs.addAll(externsFiles.collect() { SourceFile.fromFile(it) })
         }
         List<SourceFile> inputs = new ArrayList<SourceFile>()
-        inputFiles.each { inputFile -> 
+        inputFiles.each { inputFile ->
           inputs.add(SourceFile.fromFile(inputFile))
         }
         Result result = compiler.compile(externs, inputs, options)
@@ -42,7 +43,7 @@ class JsMinifier {
               sourceMap.write(sourceMapContent.toString())
             }
         } else {
-        	String error = ""
+            String error = ""
             result.errors.each {
                 error += "${it.sourceName}:${it.lineNumber} - ${it.description}\n"
             }
